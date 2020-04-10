3c652463ae6b1523e76b58c25bde397011d63f7d
ChrisAtMakeGoodTech <makegoodtech@gmail.com>
2020-04-10 16:11:30 -0500

    countdown timer

--- code.js
+++ code.js
@@ -1,10 +1,71 @@
 import Period from './Period.js';
 import setupNotifications from './setupNotifications.js';
+import { openDB, deleteDB, wrap, unwrap } from 'https://unpkg.com/idb?module';
+
+/**
+UI
+	buttons
+	log display
+	notifications
+		notification audio
+	work period setup
+	audio setup
+*/
+
+/*
+work period
+	add
+		name
+		length
+		sound
+		remind time
+	edit
+	start
+	end
+	delete
+
+audio
+	add
+	remove
+	linked periods
+
+log
+	store
+	read
+	display
+
+notifications
+	timeout
+	cancel
+	show
+	repeat
+	actions
+
+idb
+	periods
+	audio
+	log
+*/
 
 setupNotifications();
 
 const OutputSection = document.getElementById('output');
 const StartButtons = document.getElementById('start-buttons');
+const Counter = document.getElementById('counter');
+
+// const TimerApp = {};
+// function setUpButtons(){
+// 	TimerApp.periods.forEach(p => {
+// 		// add button to UI
+// 	});
+// }
+
+// setUpButtons();
+
+// TimerApp.addEventListener('period-edit', function(){
+// 	// clear buttons
+// 	setUpButtons();
+// });
 
 const msInMinute = 60000;
 const Periods = [
@@ -13,6 +74,7 @@ const Periods = [
 ];
 
 let Timeout = null;
+let TimeEnd = null;
 
 window.addEventListener('beforeunload', function (e) {
 	if (Timeout !== null) {
@@ -36,9 +98,38 @@ Periods.forEach(p => {
 			addOutput(`${p.Name} period ended.`);
 			new Notification(`${p.Name} period has ended.`);
 		}, p.LengthMilliseconds);
+
+		TimeEnd = End.getTime();
+
+		doTimer();
+
 	});
 	StartButtons.appendChild(NewButton);
 });
+
+function doTimer() {
+	if (TimeEnd === null) {
+		Counter.textContent = '';
+		return;
+	}
+
+	const TimeTil = TimeEnd - new Date().getTime();
+	if (TimeTil <= 0){
+		Counter.textContent = '';
+		return;
+	}
+
+	const TotalSecondsTil = Math.floor(TimeTil / 1000);
+
+	const HoursTil = Math.floor(TotalSecondsTil / (60 * 60));
+	const MinutesTil = Math.floor((TotalSecondsTil / 60) - (HoursTil * 60));
+	const SecondsTil = Math.floor(TotalSecondsTil - ((HoursTil * 60 * 60) + (MinutesTil * 60)));
+
+	Counter.textContent = `${String(HoursTil).padStart(2, '0')}:${String(MinutesTil).padStart(2, '0')}:${String(SecondsTil).padStart(2, '0')}`;
+
+	setTimeout(doTimer, 1000);
+
+}
 
 {
 	const NewButton = document.createElement('button');
@@ -54,6 +145,7 @@ function clearExistingPeriod() {
 		addOutput(`Cleared existing period.`);
 		clearTimeout(Timeout);
 		Timeout = null;
+		TimeEnd = null;
 	}
 }
 
