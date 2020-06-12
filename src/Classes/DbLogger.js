ab03078b10bd0cd1d5e421a7ac487a70295aa28e
ChrisAtMakeGoodTech <makegoodtech@gmail.com>
2020-06-11 20:12:37 -0500

    add idb package

--- src/Classes/DbLogger.js
+++ src/Classes/DbLogger.js
@@ -1,4 +1,4 @@
-// import { openDB, deleteDB, wrap, unwrap } from 'https://unpkg.com/idb?module';
+import { openDB, deleteDB, wrap, unwrap } from 'idb';
 
 const Statics = {
 	DbName: 'TimersDb',
@@ -10,24 +10,24 @@ const Statics = {
 let Db;
 
 async function OpenDb() {
-	// Db = await openDB(Statics.DbName, Statics.DbVersion, {
-	// 	upgrade(db, oldVersion, newVersion, transaction) {
-	// 		const Store = db.createObjectStore(Statics.DbStoreName, {
-	// 			keyPath: Statics.DbKeyPath,
-	// 			autoIncrement: true,
-	// 		});
-	// 		Store.createIndex('date', 'date');
-	// 	},
-	// 	blocked() {
-	// 		// …
-	// 	},
-	// 	blocking() {
-	// 		// …
-	// 	},
-	// 	terminated() {
-	// 		// …
-	// 	},
-	// });
+	Db = await openDB(Statics.DbName, Statics.DbVersion, {
+		upgrade(db, oldVersion, newVersion, transaction) {
+			const Store = db.createObjectStore(Statics.DbStoreName, {
+				keyPath: Statics.DbKeyPath,
+				autoIncrement: true,
+			});
+			Store.createIndex('date', 'date');
+		},
+		blocked() {
+			// …
+		},
+		blocking() {
+			// …
+		},
+		terminated() {
+			// …
+		},
+	});
 }
 
 OpenDb();
@@ -44,12 +44,12 @@ export default class DbLogger {
 	}
 
 	async addLog(line) {
-		// const id = await Db.add(Statics.DbStoreName, { text: line, date: new Date() });
-		// localStorage.setItem(`wrote-db-${Statics.DbName}-${Statics.DbStoreName}`, id);
+		const id = await Db.add(Statics.DbStoreName, { text: line, date: new Date() });
+		localStorage.setItem(`wrote-db-${Statics.DbName}-${Statics.DbStoreName}`, id);
 	}
 	async getLog(id) {
-		// const val = await Db.get(Statics.DbStoreName, id);
-		// console.log(id, val);
-		// return val;
+		const val = await Db.get(Statics.DbStoreName, id);
+		console.log(id, val);
+		return val;
 	}
 }
