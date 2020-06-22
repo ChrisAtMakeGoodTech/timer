4146aea451c79d723dc73487a7489cdc0a9ec3ea
ChrisAtMakeGoodTech <makegoodtech@gmail.com>
2020-06-22 18:06:44 -0500

    add IMessageHandlers
    Setting the index type allows me to remove
    a bunch of ts-ignore comments.

--- ts/functions/messageHandler.ts
+++ ts/functions/messageHandler.ts
@@ -2,9 +2,7 @@ import MessageHandlers from '../objects/MessageHandlers';
 
 export default function messageHandler(event: MessageEvent) {
 	if (typeof event.data === 'object' && event.data !== null && typeof event.data.type === 'string') {
-		// @ts-ignore
 		if (typeof MessageHandlers[event.data.type] === 'function') {
-			// @ts-ignore
 			MessageHandlers[event.data.type](event);
 		} else {
 			console.error('Unknown message type from service worker: ' + event.data.type);
